555583acc001bf8a6e125b405a284ab49c59ec45
Rowan Cannaday <cannadayr@gmail.com>
2018-12-06 11:57:43 -0600

    add vector iteration example

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <config.h>
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main() {
     std::cout << "INFORMATION:\n"
               << "\tName: " << PACKAGE_NAME << "\n"
               << "\tVersion: " << PACKAGE_VERSION << "\n"
-              << "\tURL: " << PACKAGE_URL << "\n";
+              << "\tURL: " << PACKAGE_URL << std::endl << std::endl;
+
+    // 1.)
+    std::cout << "1.) initiate a vector on the stack and iterate over" << std::endl;
+    static std::vector<std::string> str_vec;
+    str_vec = {"Heres","some","words","to","iterate","over"};
+
+    for (auto &i : str_vec) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl << std::endl;
+
     return 0;
 }
