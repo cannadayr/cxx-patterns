88ee2f33bbc394194dc90e0982c30adfb6232fb7
Rowan Cannaday <cannadayr@gmail.com>
2018-12-06 12:38:40 -0600

    only using endl to flush the stream

--- src/main.cpp
+++ src/main.cpp
@@ -17,22 +17,22 @@ int main(int argc, char** argv) {
     std::cout << "INFORMATION:\n"
               << "\tName: " << PACKAGE_NAME << "\n"
               << "\tVersion: " << PACKAGE_VERSION << "\n"
-              << "\tURL: " << PACKAGE_URL << std::endl << std::endl;
+              << "\tURL: " << PACKAGE_URL << "\n" << std::endl;
 
     // 1.)
-    std::cout << "1.) initiate a vector on the stack and iterate over" << std::endl;
+    std::cout << "1.) initiate a vector on the stack and iterate over\n";
     static std::vector<std::string> str_vec;
     str_vec = {"Heres","some","words","to","iterate","over"};
 
     for (auto &i : str_vec) {
         std::cout << i << " ";
     }
-    std::cout << std::endl << std::endl;
+    std::cout << "\n" << std::endl;
 
     // 2.)
-    std::cout << "2.) initialize a struct on the heap and call method to print its properties" << std::endl;
+    std::cout << "2.) initialize a struct on the heap and call method to print its properties\n";
     std::unique_ptr<Entity> entity1 = std::make_unique<Entity>(1,"fred");
-    std::cout << *entity1 << std::endl;
+    std::cout << *entity1 << "\n" << std::endl;
 
     return 0;
 }
