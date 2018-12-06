88a78c45c57ced4cfd0078ebff2ac7765086c57a
Rowan Cannaday <cannadayr@gmail.com>
2018-12-06 11:35:39 -0600

    output pkg information in main, use c++11
    autotools clean up & comments, use m4/ for macros
    change binary name to cxx-patterns

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#include <config.h>
 #include <iostream>
 
 int main() {
-    std::cout << "hello!\n";
+    std::cout << "INFORMATION:\n"
+              << "\tName: " << PACKAGE_NAME << "\n"
+              << "\tVersion: " << PACKAGE_VERSION << "\n"
+              << "\tURL: " << PACKAGE_URL << "\n";
     return 0;
 }
