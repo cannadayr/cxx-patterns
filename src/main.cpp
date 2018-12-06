23813e0663a9e85373c6a9028d2279750454a2b4
Rowan Cannaday <cannadayr@gmail.com>
2018-12-06 15:49:24 -0600

    add a queue and thread example

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <queue>
+#include <thread>
 
 struct Entity {
     int id;
@@ -33,6 +35,35 @@ int main(int argc, char** argv) {
     std::cout << "2.) initialize a struct on the heap and call method to print its properties\n";
     std::unique_ptr<Entity> entity1 = std::make_unique<Entity>(1,"fred");
     std::cout << *entity1 << "\n" << std::endl;
+
+    // 3.)
+    std::cout << "3.) push elements from a queue and then flush\n";
+    std::queue<std::string> queue1;
+    queue1.push("elem1");
+    queue1.push("elem2");
+    queue1.push("elem3");
+    while (!queue1.empty()) {
+        std::string queue1_str = queue1.front();
+        std::cout << queue1_str << "\n";
+        queue1.pop();
+    }
+    std::cout << std::endl;
+
+
+    // 4.)
+    std::cout << "4.) run two processes in parallel, counting from 0-9. not thread safe.\n";
+    std::thread thread1([](){
+        for (int i = 0; i < 10; i++) {
+            std::cout << "thread1: " << i << "\n";
+        }
+    });
+    std::thread thread2([](){
+        for (int i = 0; i < 10; i++) {
+            std::cout << "thread2: " << i << "\n";
+        }
+    });
+    thread1.join();
+    thread2.join();
 
     return 0;
 }
